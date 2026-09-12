debfecda20a58ed97cc4511216dcd0e4567cdeb7
hammadqureshi31 <hammad2004qureshi@gmail.com>
2026-09-12 22:11:48 +0500

    feat: deploy chatbot to EKS with Jenkins pipeline

--- Chatbot-UI/components/Chat/ChatMessage.tsx
+++ Chatbot-UI/components/Chat/ChatMessage.tsx
@@ -2,7 +2,8 @@ import { Message } from '@/types/chat';
 import { IconCheck, IconCopy, IconEdit, IconUser, IconRobot } from '@tabler/icons-react';
 import { useTranslation } from 'next-i18next';
 import { FC, memo, useEffect, useRef, useState } from 'react';
-import rehypeMathjax from 'rehype-mathjax';
+import rehypeKatex from 'rehype-katex';
+import 'katex/dist/katex.min.css';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import { CodeBlock } from '../Markdown/CodeBlock';
@@ -173,8 +174,8 @@ export const ChatMessage: FC<Props> = memo(
                 <MemoizedReactMarkdown
                   className="prose dark:prose-invert"
                   remarkPlugins={[remarkGfm, remarkMath]}
-                  rehypePlugins={[rehypeMathjax]}
-                  components={{
+                  rehypePlugins={[rehypeKatex]}                  
+		  components={{
                     code({ node, inline, className, children, ...props }) {
                       const match = /language-(\w+)/.exec(className || '');
 
